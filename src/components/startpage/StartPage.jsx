99618efa4827111630970d122b4f52af9d19052d
suno Choi <vlfxhd@naver.com>
2023-01-12 15:30:35 +0900

    feat: startpage 생성

--- src/components/startpage/StartPage.jsx
+++ src/components/startpage/StartPage.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 import { ReactComponent as Logo } from "../../img/logo/logo-graphic.svg";
+import { useNavigate } from "react-router-dom";
 
 const StartPage = () => {
+  const navigate = useNavigate();
+  const StartHandler = () => {
+    navigate("/login");
+  };
   return (
     <StartContainer>
       <MainLogo />
-      <div>Bullet Box</div>
+      Bullet Box
+      <StartBtn
+        onClick={() => {
+          StartHandler();
+        }}
+      >
+        Start
+      </StartBtn>
     </StartContainer>
   );
 };
@@ -15,13 +27,27 @@ export default StartPage;
 
 const StartContainer = styled.div`
   display: flex;
-  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   background-color: var(--color-main);
   height: 100vh;
+  font-size: 1.5rem;
+  color: white;
 `;
 
 const MainLogo = styled(Logo)`
   fill: white;
   width: 50%;
   height: 50vh;
+`;
+
+const StartBtn = styled.button`
+  margin-top: 15%;
+  font-size: 1.5rem;
+  background-color: white;
+  color: var(--color-main);
+  width: 55%;
+  height: 6.7vh;
+  border: none;
+  border-radius: 8px;
 `;
