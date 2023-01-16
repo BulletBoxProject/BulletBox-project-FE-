82e68cb5f9c07e98a2932dd4d3c8d502bccfe80e
suno Choi <vlfxhd@naver.com>
2023-01-16 23:43:17 +0900

    remove: Header 뒤로가기,알람,메뉴 삭제

--- src/layout/header/screen/Header.jsx
+++ src/layout/header/screen/Header.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-import DropdownMenu from "../components/DropdownMenu";
 import { ReactComponent as logoIcon } from "../../../img/logo/logo-type-bold.svg";
-import { ReactComponent as backIcon } from "../../../img/navi/back.svg";
-import { ReactComponent as bellX } from "../../../img/navi/bell-x.svg";
 
 const Header = () => {
   const navigate = useNavigate();
-  const handleGoBack = () => {
-    navigate(-1);
-  };
 
   const onClickHandler = () => {
     navigate("/");
@@ -18,14 +12,6 @@ const Header = () => {
 
   return (
     <HeaderDiv>
-      <HeaderBtn>
-        <Back
-          onClick={() => {
-            handleGoBack();
-          }}
-        />
-      </HeaderBtn>
-
       <HeaderBtn
         onClick={() => {
           onClickHandler();
@@ -33,10 +19,6 @@ const Header = () => {
       >
         <Logo />
       </HeaderBtn>
-      <HeaderLeftDiv>
-        <Bell />
-        <DropdownMenu />
-      </HeaderLeftDiv>
     </HeaderDiv>
   );
 };
@@ -56,22 +38,4 @@ const HeaderBtn = styled.button`
 const Logo = styled(logoIcon)`
   margin-left: 10%;
   width: 30%;
-`;
-
-const HeaderLeftDiv = styled.div`
-  display: flex;
-  align-items: center;
-  width: 28%;
-`;
-
-const Back = styled(backIcon)`
-  width: 98%;
-  height: 3.3vh;
-  color: var(--color-main);
-`;
-
-const Bell = styled(bellX)`
-  width: 88%;
-  height: 2.5vh;
-  color: var(--color-main);
 `;
