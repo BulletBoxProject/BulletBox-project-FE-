e28ce0c3ce6432e2fc66ce0137356ded6c2f411f
pablaw <chulhwa7@gmail.com>
2023-01-17 00:13:57 +0900

    design: 데일리로그 추가 페이지 레이아웃 작업

--- src/components/dailyLog/add/components/AddTodoInput.jsx
+++ src/components/dailyLog/add/components/AddTodoInput.jsx
@@ -2,19 +2,24 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 import { BsFillPlusCircleFill } from "react-icons/bs";
+import { RxTriangleDown } from "react-icons/rx";
 
 import ToggleSwitch from "../../../common/ToggleSwitch";
 
 const AddTodoInput = () => {
   const [showBullet, setShowBullet] = useState(false);
-  const [addBulletTodo, setAddBulletTodo] = useState({ bullet: "", todo: "" });
+  const [showTimeSet, setShowTimeSet] = useState(false);
+  const [addBulletTodo, setAddBulletTodo] = useState({
+    todoContent: "",
+    todoBulletName: "",
+  });
   const [addMemo, setAddMemo] = useState({ state: false, memos: [] });
   console.log(addBulletTodo);
   const showBulletHandler = () => {
     setShowBullet(!showBullet);
   };
   const selectBulletHandler = (e) => {
-    setAddBulletTodo({ ...addBulletTodo, bullet: e.target.value });
+    setAddBulletTodo({ ...addBulletTodo, todoBulletName: e.target.value });
     setShowBullet(!showBullet);
   };
   const addTodoHandler = () => {
@@ -24,6 +29,15 @@ const AddTodoInput = () => {
     console.log();
     setAddMemo({ ...addMemo, state: !addMemo.state });
   };
+  const bulletMemoHandler = (e) => {
+    console.log("불렛메모", e.target.value);
+  };
+  const showSetTimeHandler = () => {
+    setShowTimeSet(!showTimeSet);
+  };
+  const categorySelectHandler = (e) => {
+    console.log(e.target.value);
+  };
   return (
     <Container>
       <BulletTodoForm>
@@ -32,20 +46,20 @@ const AddTodoInput = () => {
         </BulletSelectButton>
         {showBullet ? (
           <BulletList>
-            <option onClick={selectBulletHandler} value={"bullet1"}>
-              bullet1
+            <option onClick={selectBulletHandler} value={"todo"}>
+              todo
             </option>
-            <option onClick={selectBulletHandler} value={"bullet2"}>
-              bullet2
+            <option onClick={selectBulletHandler} value={"complete"}>
+              complete
             </option>
-            <option onClick={selectBulletHandler} value={"bullet3"}>
-              bullet3
+            <option onClick={selectBulletHandler} value={"postpone"}>
+              postpone
             </option>
-            <option onClick={selectBulletHandler} value={"bullet4"}>
-              bullet4
+            <option onClick={selectBulletHandler} value={"important"}>
+              important
             </option>
-            <option onClick={selectBulletHandler} value={"bullet5"}>
-              bullet5
+            <option onClick={selectBulletHandler} value={"memo"}>
+              memo
             </option>
           </BulletList>
         ) : null}
@@ -57,7 +71,11 @@ const AddTodoInput = () => {
       <AddMemoDiv>
         {addMemo.state ? (
           <AddMemoInputDiv>
-            <AddMemoInput />
+            <AddMemoInput
+              type="text"
+              placeholder="불렛메모를 추가하세요"
+              onChange={bulletMemoHandler}
+            />
             <MemoSubmitButton type="button">추가</MemoSubmitButton>
           </AddMemoInputDiv>
         ) : null}
@@ -67,12 +85,63 @@ const AddTodoInput = () => {
       </AddMemoDiv>
       <hr />
       <TimeSettingDiv>
-        <ToggleDiv>
-          <span>off</span>
-          <ToggleSwitch />
-          <span>on</span>
-        </ToggleDiv>
+        <TimeSetOnOffDiv>
+          <SwitchDiv>
+            <OnOffSwitchButton type="button" onClick={showSetTimeHandler}>
+              {showTimeSet ? "ON" : "OFF"}
+            </OnOffSwitchButton>
+          </SwitchDiv>
+          {showTimeSet ? null : <hr style={{ width: " 100%" }} />}
+          {showTimeSet ? (
+            <SetTimeSelectDiv>
+              <SelectHoursDiv>
+                <SelectHoursButton>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <SelectDownIcon />
+                </SelectHoursButton>
+                <span>시</span>
+              </SelectHoursDiv>
+              <SelectMinutesDiv>
+                <SelectMinutesButton>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <SelectDownIcon />
+                </SelectMinutesButton>
+                <span>분</span>
+              </SelectMinutesDiv>
+            </SetTimeSelectDiv>
+          ) : null}
+          {showTimeSet ? <hr style={{ width: " 100%" }} /> : null}
+        </TimeSetOnOffDiv>
       </TimeSettingDiv>
+      <CategorySelectDiv>
+        <CategoryButton
+          type="button"
+          value="카테고리1"
+          onClick={categorySelectHandler}
+        >
+          카테고리1
+        </CategoryButton>
+        <CategoryButton type="button" value="카테고리2">
+          카테고리2
+        </CategoryButton>
+        <CategoryButton type="button" value="카테고리3">
+          카테고리3
+        </CategoryButton>
+        <CategoryButton type="button" value="카테고리4">
+          카테고리4
+        </CategoryButton>
+        <CategoryButton type="button" value="카테고리5">
+          카테고리5
+        </CategoryButton>
+        <CategoryButton type="button" value="카테고리6">
+          카테고리6
+        </CategoryButton>
+      </CategorySelectDiv>
+      <hr />
+      <AddInputButtonGroup>
+        <CancleButton type="button">취소</CancleButton>
+        <SubmitButton type="button">확인</SubmitButton>
+      </AddInputButtonGroup>
     </Container>
   );
 };
@@ -126,9 +195,68 @@ const AddMemoIcon = styled(BsFillPlusCircleFill)`
 const TimeSettingDiv = styled.div`
   width: 100%;
 `;
-const ToggleDiv = styled.div`
+const TimeSetOnOffDiv = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: flex-end;
   align-items: center;
   gap: 5px;
+`;
+const SwitchDiv = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+`;
+const OnOffSwitchButton = styled.button`
+  display: flex;
+  justify-self: flex-end;
+`;
+const SetTimeSelectDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 60%;
+`;
+const SelectHoursDiv = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+const SelectHoursButton = styled.button``;
+const SelectDownIcon = styled(RxTriangleDown)`
+  width: 15px;
+  height: 15px;
+`;
+const SelectMinutesDiv = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+const SelectMinutesButton = styled.button``;
+const CategorySelectDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 10px auto;
+  flex-wrap: wrap;
+  gap: 10px;
+  width: 80%;
+`;
+const CategoryButton = styled.button`
+  width: 30%;
+  height: 3em;
+`;
+const AddInputButtonGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  width: 40%;
+`;
+const CancleButton = styled.button`
+  width: 30%;
+  height: 3em;
+`;
+const SubmitButton = styled.button`
+  width: 30%;
+  height: 3em;
+  background-color: var(--color-main);
+  border: 0;
 `;
