dc1d7ae1a4e93c0e6a9b890fc07224303604a4a2
pablaw <chulhwa7@gmail.com>
2023-01-20 00:33:15 +0900

    design: 데일리로그 추가 버튼 크기 및 위치 조정

--- src/components/dailyLog/screen/DailyLogContainer.jsx
+++ src/components/dailyLog/screen/DailyLogContainer.jsx
@@ -135,23 +135,23 @@ const MoreIcon = styled(moreIcon)`
   height: 24px;
 `;
 const AddTodoDiv = styled.div`
-  display: flex;
-  position: relative;
-  justify-content: center;
+  position: fixed;
+  left: 85vw;
+  top: 86vh;
 `;
 const AddTodoButton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 24px;
-  height: 24px;
+  /* width: 24px;
+  height: 24px; */
   border: 0;
   background-color: transparent;
 `;
 const AddTodoIcon = styled(BsFillPlusCircleFill)`
   fill: var(--color-main);
-  width: 24px;
-  height: 24px;
+  width: 40px;
+  height: 40px;
 `;
 const AddSelectDiv = styled.div`
   display: flex;
