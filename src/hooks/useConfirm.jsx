6231f883b840d60fb4159c58d1012e9b5943cc49
suno Choi <vlfxhd@naver.com>
2023-01-21 05:51:13 +0900

    feat: logout confirm창

---
+++ src/hooks/useConfirm.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const useConfirm = (message = null, onConfirm, onCancel) => {
+  if (!onConfirm || typeof onConfirm !== "function") {
+    return;
+  }
+  if (onCancel && typeof onCancel !== "function") {
+    return;
+  }
+
+  const confirmAction = () => {
+    if (window.confirm(message)) {
+      onConfirm();
+    } else {
+      onCancel();
+    }
+  };
+
+  return confirmAction;
+};
+
+export default useConfirm;
