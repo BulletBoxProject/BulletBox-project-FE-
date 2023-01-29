ea305bddfdf2c83937bf0763141ab56012ef4150
suno Choi <vlfxhd@naver.com>
2023-01-29 15:17:07 +0900

    design: scrollbar change

--- src/shared/GlobalStyle.jsx
+++ src/shared/GlobalStyle.jsx
@@ -52,7 +52,14 @@ a{
   cursor: pointer;
 }
 &::-webkit-scrollbar {
-    display: none;
+    width: 4px;
+    height: 4px;
+    border-radius: 6px;
+    background: rgba(255, 255, 255, 0.4);
+  }
+  &::-webkit-scrollbar-thumb {
+    background: rgba(0, 0, 0, 0.3);
+    border-radius: 6px;
   }
 
 `;
