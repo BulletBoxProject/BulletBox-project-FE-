c84de9a2ef0129b343b71ec12e2daa164d5778d1
suno Choi <vlfxhd@naver.com>
2023-02-02 03:50:28 +0900

    #115 design: search bullet 추가

--- src/components/search/components/SearchTodoBullet.jsx
+++ src/components/search/components/SearchTodoBullet.jsx
@@ -6,36 +6,56 @@ import { ReactComponent as checkBullet } from "../../../img/bullet/check-2.svg";
 import { ReactComponent as postphoneBullet } from "../../../img/bullet/postpone-3.svg";
 import { ReactComponent as memoBullet } from "../../../img/bullet/memo-5.svg";
 import { ReactComponent as importantBullet } from "../../../img/bullet/asterisk-6.svg";
-import { ReactComponent as favoriteBullet } from "../../../img/bullet/star-7.svg";
+import { ReactComponent as favoriteBullet } from "../../../img/bullet/round-star-outline.svg";
+import { ReactComponent as undefinedBullet } from "../../../img/bullet/triangle.svg";
+import { ReactComponent as highLightBullet } from "../../../img/bullet/circle-double.svg";
 
 const SearchTodoBullet = ({ bulletName }) => {
   const selectBullet = () => {
     switch (bulletName) {
-      case "TODO":
+      case "할 일":
         return (
           <div>
             <TodoBullet />
           </div>
         );
-      case "COMPlETE":
+      case "완료":
         return (
           <div>
             <CheckBullet />
           </div>
         );
-      case "DELAYED":
+      case "미룬 일":
         return (
           <div>
             <PostphoneBullet />
           </div>
         );
-      case "IMPORTANT":
+      case "중요":
         return (
           <div>
             <ImportantBullet />
           </div>
         );
-      case "MEMO":
+      case "자주":
+        return (
+          <div>
+            <FavoriteBullet />
+          </div>
+        );
+      case "미정":
+        return (
+          <div>
+            <UndefinedBullet />
+          </div>
+        );
+      case "강조":
+        return (
+          <div>
+            <HighLightBullet />
+          </div>
+        );
+      case "메모":
         return (
           <div>
             <MemoBullet />
@@ -71,6 +91,15 @@ const MemoBullet = styled(memoBullet)`
 `;
 
 const FavoriteBullet = styled(favoriteBullet)`
+  width: 24px;
+  height: 24px;
+`;
+
+const UndefinedBullet = styled(undefinedBullet)`
+  width: 24px;
+  height: 24px;
+`;
+const HighLightBullet = styled(highLightBullet)`
   width: 24px;
   height: 24px;
 `;
