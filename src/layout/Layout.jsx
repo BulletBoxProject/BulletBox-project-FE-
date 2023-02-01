06eaac3fde6f6b265c44d2ae369559c91b81e100
pablaw <chulhwa7@gmail.com>
2023-02-01 13:21:50 +0900

    design: 전체 레이아웃 스타일링, height 수정, body 배경색 변경

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -27,7 +27,7 @@ const Container = styled.div`
   justify-content: center;
   flex-direction: column;
   width: 100%;
-  height: 100vh;
+  height: 100%;
   background-color: var(--color-light-gray);
 `;
 const PageContainer = styled.div`
