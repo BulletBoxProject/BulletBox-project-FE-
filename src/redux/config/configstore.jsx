b97e4b6ce29ad6debfaa3e3ab4f433afce957e19
suno Choi <vlfxhd@naver.com>
2023-02-19 04:27:45 +0900

    rename: configstore 수정

--- src/redux/config/configstore.jsx
+++ src/redux/config/configstore.jsx
@@ -1,20 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import emotionDiary from "../modules/emotionDiarySlice";
 import dailyTodo from "../modules/dailysSlice";
 import category from "../modules/categorySlice";
 import favorite from "../modules/favoriteSlice";
 import search from "../modules/searchSlice";
 import mainTodo from "../modules/mainSlice";
-import emotionDiary from "../modules/emotionDiarySlice";
 
 const store = configureStore({
   reducer: {
+    emotionDiary,
     dailyTodo,
     category,
     favorite,
     search,
     mainTodo,
-    emotionDiary,
   },
 });
 
